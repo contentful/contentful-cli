39eba464ed81dd94949237b071bb5eaf977e5daf
Benedikt Rötsch <benedikt@contentful.com>
2017-05-12 14:49:12 +0200

    fix(emojis): do not generate numbered emojis on non-osx environments

--- test/utils/emoji.test.js
+++ test/utils/emoji.test.js
@@ -10,13 +10,19 @@ import {
 
 const emojis = [welcomeEmoji, successEmoji, errorEmoji, infoEmoji]
 
-test('emojis should have space at the end to avoid display issues', (t) => {
+test.serial('emojis should have space at the end to avoid display issues', (t) => {
   emojis.forEach((emoji) => {
     t.is(emoji.charAt(emoji.length - 1), ' ', `Emoji ${emoji} has a space at the end`)
   })
 })
 
-test('numbered emojis', (t) => {
-  const numberEmoji = generateNumberEmoji(123)
-  t.is((numberEmoji.match(/ /g) || []).length, 3, 'Generated emoji number has spaces inbetween to avoid display issues')
+test.serial('numbered emojis osx', (t) => {
+  if (process.platform === 'darwin') {
+    const numberEmoji = generateNumberEmoji(123)
+    t.is((numberEmoji.match(/ /g) || []).length, 3, 'Generated emoji number has spaces inbetween to avoid display issues')
+  } else {
+    const numbers = generateNumberEmoji(123)
+    t.is(numbers, '123')
+    t.is((numbers.match(/ /g) || []).length, 0, 'Generated number has no spaces since nothing got replaced by emojis')
+  }
 })
