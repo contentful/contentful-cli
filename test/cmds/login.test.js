7c1c377cc87549b44d0ef7a09e6d016d4b21d1a8
Benedikt Rötsch <benedikt@contentful.com>
2017-05-19 17:17:25 +0200

    refactor(guide): run dev server from within the guide

--- test/cmds/login.test.js
+++ test/cmds/login.test.js
@@ -52,22 +52,20 @@ test.serial('login - without error', async (t) => {
   t.is(writeFileStub.args[0][1], JSON.stringify(mockedRcConfig, null, 2) + '\n', 'stores entered token')
 })
 
-test.serial('login - with retry', async (t) => {
+test.serial('login - user abort', async (t) => {
   promptStub.onCall(0).returns({ready: false})
-  promptStub.onCall(1).returns({ready: true})
-  promptStub.onCall(2).returns({cmaToken: 'mockedToken'})
 
   emptyContext()
   await loginHandler()
 
-  t.true(opnStub.calledOnce, 'opened the browser once')
-  const mockedRcConfig = {
-    cmaToken: 'mockedToken'
-  }
-  t.is(writeFileStub.args[0][1], JSON.stringify(mockedRcConfig, null, 2) + '\n', 'stores entered token')
+  t.true(statStub.called, 'did load rc config')
+  t.is(promptStub.callCount, 1, 'did ask once with inquirer')
+  t.true(opnStub.notCalled, 'did not try to open a browser')
+  t.true(writeFileStub.notCalled, 'did not try to store rc')
 })
 
 test.serial('login - already logged in', async (t) => {
+  emptyContext()
   setContext({cmaToken: 'alreadyLoggedIn'})
   await loginHandler()
 
--- test/utils/actions.test.js
+++ test/utils/actions.test.js
@@ -4,7 +4,6 @@ import inquirer from 'inquirer'
 
 import {
   confirmation,
-  reoccurringConfirmation,
   __RewireAPI__ as actionsRewireAPI
 } from '../../lib/utils/actions'
 
@@ -34,17 +33,4 @@ test.serial('confirmation is asked again when user denies', async (t) => {
   const confirmationResult = await confirmation()
   t.is(promptStub.callCount, 1, 'question was asked twice')
   t.false(confirmationResult, 'returns false when user declined')
-})
-
-test.serial('recurring confirmation continues after user accepted', async (t) => {
-  promptStub.onCall(0).resolves({ ready: true })
-  await reoccurringConfirmation()
-  t.is(promptStub.callCount, 1, 'question was asked once')
-})
-
-test.serial('recurring confirmation is asked again when user denies', async (t) => {
-  promptStub.onCall(0).resolves({ ready: false })
-  promptStub.onCall(1).resolves({ ready: true })
-  await reoccurringConfirmation()
-  t.is(promptStub.callCount, 2, 'question was asked twice')
 })
