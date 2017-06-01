70e4342fd61e67ff0a79fa6c11cd3e69aa9901d6
Julia Friesel <julia.friesel@gmail.com>
2017-06-01 15:23:13 +0200

    test(patch): Add tests for publishPatchResults

--- test/unit/cmds/space_cmds/patch-handler.test.js
+++ test/unit/cmds/space_cmds/patch-handler.test.js
@@ -3,7 +3,10 @@ import sinon from 'sinon'
 import Bluebird from 'bluebird'
 
 import { successEmoji } from '../../../../lib/utils/emojis'
-import patchHandler from '../../../../lib/cmds/space_cmds/patch-handler'
+import patchHandler, {
+  publishPatchResults,
+  __RewireAPI__ as publishRewire
+} from '../../../../lib/cmds/space_cmds/patch-handler'
 import stubContentType from '../content-type_cmds/stubs/_content-type'
 
 const loggingStubs = () => ({ log: sinon.spy(), error: sinon.spy() })
@@ -176,7 +179,8 @@ test('it logs if the patches changed the Content Type', async function (t) {
     'path/b': {id: '123', action: 'patch', patches: [{op: 'add', path: '/name', value: 'hello there'}]}
   }
   const helpers = {
-    readPatchFile: (path) => patches[path]
+    readPatchFile: (path) => patches[path],
+    confirm: sinon.stub()
   }
   const spaceStub = {
     getContentType: sinon.stub().returns(Bluebird.resolve('test content type'))
@@ -205,7 +209,8 @@ test('it logs if the patches did not change the Content Type', async function (t
     'path/b': {id: '123', action: 'patch', patches: [{op: 'add', path: '/name', value: 'hello there'}]}
   }
   const helpers = {
-    readPatchFile: (path) => patches[path]
+    readPatchFile: (path) => patches[path],
+    confirm: sinon.stub()
   }
   const spaceStub = {
     getContentType: sinon.stub().returns(Bluebird.resolve({name: 'test content type'}))
@@ -228,7 +233,7 @@ test('it logs if the patches did not change the Content Type', async function (t
   t.true(logging.log.calledWith('No changes for content type "test content type"'))
 })
 
-test('it does not log anything when --dry-run', async function (t) {
+test('it does not log or publish  anything when --dry-run', async function (t) {
   const patches = {
     'path/a': {id: '123', action: 'patch', patches: ['beep', 'boop']},
     'path/b': {id: '123', action: 'patch', patches: [{op: 'add', path: '/name', value: 'hello there'}]}
@@ -248,6 +253,7 @@ test('it does not log anything when --dry-run', async function (t) {
     patchFilePaths: Object.keys(patches),
     dryRun: true
   }
+  const publishStub = sinon.stub()
   const logging = loggingStubs()
   const applyPatchesSpy = sinon.stub().returns({patched: false})
 
@@ -256,4 +262,92 @@ test('it does not log anything when --dry-run', async function (t) {
   t.true(logging.log.called)
   t.false(logging.log.calledWith('No changes for Content Type "CT"'))
   t.false(logging.log.calledWith(`${successEmoji} Patches applied`))
+  t.false(publishStub.called)
+})
+
+test('publishes patch results in the end', async function (t) {
+  const publishStub = sinon.stub()
+  publishRewire.__Rewire__('publishPatchResults', publishStub)
+  const patches = {
+    'path/a': {id: '123', action: 'patch', patches: ['beep', 'boop']},
+    'path/b': {id: '123', action: 'patch', patches: [{op: 'add', path: '/name', value: 'hello there'}]}
+  }
+  const helpers = {
+    readPatchFile: (path) => patches[path]
+  }
+  const spaceStub = {
+    getContentType: sinon.stub().returns(Bluebird.resolve('test content type'))
+  }
+  const createContentfulClient = () => {
+    return {
+      getSpace: sinon.stub().returns(spaceStub)
+    }
+  }
+  const args = {
+    patchFilePaths: Object.keys(patches),
+    dryRun: false
+  }
+  const logging = loggingStubs()
+  const applyPatchesSpy = sinon.stub().returns({patched: false})
+
+  await patchHandler(args, createContentfulClient, applyPatchesSpy, helpers, logging)
+
+  t.true(publishStub.called)
+})
+
+test('publishes changes when user wants', async function (t) {
+  const patchResults = [
+    { patched: true, contentType: 'first' },
+    { patched: true, contentType: 'second' }
+  ]
+  const helpers = {
+    confirm: sinon.stub().returns(true)
+  }
+  const logging = loggingStubs()
+  const maybePublishContentType = sinon.stub().returns(Promise.resolve())
+
+  await publishPatchResults(patchResults, maybePublishContentType, helpers, logging)
+
+  t.is(maybePublishContentType.callCount, 2)
+  t.true(maybePublishContentType.calledWith('first'))
+  t.true(maybePublishContentType.calledWith('second'))
+
+  t.true(logging.log.called)
+  t.true(logging.log.calledWith(`${successEmoji} Patches published`))
+})
+
+test('does not publish changes when user does not confirm,', async function (t) {
+  const patchResults = [
+    { patched: true, contentType: 'first' },
+    { patched: true, contentType: 'second' }
+  ]
+  const helpers = {
+    confirm: sinon.stub().returns(false)
+  }
+  const logging = loggingStubs()
+  const maybePublishContentType = sinon.stub().returns(Promise.resolve())
+
+  await publishPatchResults(patchResults, maybePublishContentType, helpers, logging)
+
+  t.false(maybePublishContentType.called)
+
+  t.true(logging.log.called)
+  t.true(logging.log.calledWith('Your patches have been applied as drafts, not published.'))
+})
+
+test('does nothing when there were no changes', async function (t) {
+  const patchResults = [
+    { patched: false, contentType: 'first' },
+    { patched: false, contentType: 'second' }
+  ]
+  const helpers = {
+    confirm: sinon.stub().returns(true)
+  }
+  const logging = loggingStubs()
+  const maybePublishContentType = sinon.stub().returns(Promise.resolve())
+
+  await publishPatchResults(patchResults, maybePublishContentType, helpers, logging)
+
+  t.false(maybePublishContentType.called)
+  t.false(logging.log.called)
 })
