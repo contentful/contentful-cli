11d99e2d627b0cf9f5346df4f0563c08a7a093d7
Tim Beyer <tim.beyer@gmail.com>
2017-06-06 15:33:43 +0200

    fix(patch): Only load contentful-patch- prefixed files as patches
    Previously we would attempt to load every single file inside of a patch directory as a patch.
    This could lead to awkward results with hidden autogenerated files and other things.

--- test/unit/cmds/content-type_cmds/patch/helpers.test.js
+++ test/unit/cmds/content-type_cmds/patch/helpers.test.js
@@ -1,6 +1,12 @@
 import test from 'ava'
+import { join } from 'path'
+import Bluebird from 'bluebird'
 
-import { transformPath } from '../../../../../lib/cmds/content-type_cmds/patch/helpers'
+import {
+  transformPath,
+  readPatchDir,
+  __RewireAPI__ as helpersRewireAPI
+} from '../../../../../lib/cmds/content-type_cmds/patch/helpers'
 
 test('Does not transform the path when it uses an index numer', (t) => {
   const path = '/fields/0/name'
@@ -16,4 +22,28 @@ test('"transformPath" returns the path when the Content Type has no fields', (t)
   const contentType = { name: 'foo', description: 'bar' }
 
   t.is(transformPath(contentType, path), '/description')
+})
+
+test('"readPatchDir" returns all files in a path that match the file name format', async (t) => {
+  const allFiles = [
+    'some-file.json',
+    'contentful-patch-foobar-123123.json',
+    'contentful-patch-barfoo-123123.json',
+    'something-else.jpg'
+  ]
+
+  const results = [
+    join('foo', 'contentful-patch-foobar-123123.json'),
+    join('foo', 'contentful-patch-barfoo-123123.json')
+  ]
+
+  helpersRewireAPI.__Rewire__('readDir', (path) => {
+    return Bluebird.resolve(allFiles)
+  })
+
+  const patchFiles = await readPatchDir('foo')
+
+  t.deepEqual(patchFiles, results)
+
+  helpersRewireAPI.__ResetDependency__('readDir')
 })
