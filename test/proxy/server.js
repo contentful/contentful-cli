a884b9581b2eb516a61c9cf7ed6d1e983310b7c5
Stephan Schneider <stephanschndr@gmail.com>
2017-06-12 22:50:51 +0200

    fix: proxy behavior (#128)
    * fix(contentful-clients): wait for context to be read
    
    * refactor(utils/proxy): use node's url module
    
    * fix(utils/proxy): parse protocol-less proxy strings
    
    * fix(utils/proxy); use Legacy URL API
    
    * fix(utils/proyy): catch falsy auth values
    
    * test(utils/proxy): add tests for parseAuth
    
    * fix(utils/proxy): remove leading double slashes in proxyObjectToString

---
+++ test/proxy/server.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const net = require('net')
+const httpProxy = require('http-proxy')
+const url = require('url')
+
+var proxy = httpProxy.createServer()
+
+var server = http.createServer(function (req, res) {
+  console.log('Receiving reverse proxy request for:' + req.url)
+
+  proxy.web(req, res, {target: req.url, secure: false})
+}).listen(8213)
+
+server.on('connect', function (req, socket) {
+  console.log('Receiving reverse proxy request for:' + req.url)
+
+  var serverUrl = url.parse('https://' + req.url)
+
+  var srvSocket = net.connect(serverUrl.port, serverUrl.hostname, function () {
+    socket.write('HTTP/1.1 200 Connection Established\r\n' +
+    'Proxy-agent: Node-Proxy\r\n' +
+    '\r\n')
+    srvSocket.pipe(socket)
+    socket.pipe(srvSocket)
+  })
+})
+
+// Test with:
+// curl -vv -x http://127.0.0.1:8213 https://www.google.com
+// curl -vv -x http://127.0.0.1:8213 http://www.google.com
--- test/unit/utils/contentful-client-test.js
+++ test/unit/utils/contentful-client-test.js
@@ -3,10 +3,10 @@ import sinon from 'sinon'
 import {version} from '../../../package.json'
 import { createManagementClient, __RewireAPI__ as rw } from '../../../lib/utils/contentful-clients'
 
-test('set the correct application name and version', (t) => {
+test('set the correct application name and version', async (t) => {
   const rewiredCreateClient = sinon.stub()
   rw.__Rewire__('createClient', rewiredCreateClient)
-  createManagementClient({accessToken: 'accessToken'})
+  await createManagementClient({accessToken: 'accessToken'})
   t.truthy(rewiredCreateClient.args[0][0].application.match(new RegExp(`contentful.cli/${version}`, 'g')))
   t.is(rewiredCreateClient.callCount, 1)
 })
