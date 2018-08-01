d28c914ea32f19b8e443e0a0cb72213c63af4eb4
Phoebe Schmidt <phbschmidt@gmail.com>
2018-08-01 15:05:41 +0200

    Use more stubs for login file instead of readFile/writeFile

--- test/unit/cmds/login.test.js
+++ test/unit/cmds/login.test.js
@@ -6,81 +6,68 @@ import {
   handler as loginHandler,
   __RewireAPI__ as loginRewireAPI
 } from '../../../lib/cmds/login'
-import {
-  emptyContext,
-  __RewireAPI__ as contextRewireAPI
-} from '../../../lib/context'
-
-const promptStub = stub(inquirer, 'prompt')
+const mockedRcConfig = {
+  cmaToken: 'mockedToken'
+}
+const promptStub = stub(inquirer, 'prompt').returns(mockedRcConfig)
 const opnStub = stub()
-const writeFileStub = stub()
+const setContextStub = stub().resolves(true)
+const getContextStub = stub().resolves({cmaToken: false})
 
-const enoent = new Error()
-enoent.code = 'ENOENT'
-const readFileStub = stub().rejects(enoent)
 const confirmationStub = stub().resolves(true)
 
 test.before(() => {
   loginRewireAPI.__Rewire__('inquirer', inquirer)
   loginRewireAPI.__Rewire__('confirmation', confirmationStub)
   loginRewireAPI.__Rewire__('opn', opnStub)
-  contextRewireAPI.__Rewire__('readFile', readFileStub)
-  contextRewireAPI.__Rewire__('writeFile', writeFileStub)
+  loginRewireAPI.__Rewire__('setContext', setContextStub)
+  loginRewireAPI.__Rewire__('getContext', getContextStub)
+  loginRewireAPI.__Rewire__('storeRuntimeConfig', stub())
 })
 
 test.after.always(() => {
   loginRewireAPI.__ResetDependency__('inquirer')
   loginRewireAPI.__ResetDependency__('confirmation')
   loginRewireAPI.__ResetDependency__('opn')
-  contextRewireAPI.__ResetDependency__('readFile')
-  contextRewireAPI.__ResetDependency__('writeFile')
+  loginRewireAPI.__ResetDependency__('setContext')
+  loginRewireAPI.__ResetDependency__('getContext')
+  loginRewireAPI.__ResetDependency__('storeRuntimeConfig')
 })
 
 test.afterEach((t) => {
   confirmationStub.resetHistory()
   promptStub.resetHistory()
   opnStub.resetHistory()
-  readFileStub.resetHistory()
-  writeFileStub.resetHistory()
+  setContextStub.resetHistory()
+  getContextStub.resetHistory()
 })
 
 test.serial('login - without error', async (t) => {
-  const mockedRcConfig = {
-    cmaToken: 'mockedToken'
-  }
-  promptStub.returns(mockedRcConfig)
-  const setContextStub = stub().resolves(true)
-  loginRewireAPI.__Rewire__('setContext', setContextStub)
-
   await loginHandler()
 
   t.true(confirmationStub.calledOnce, 'called confirmation')
   t.true(setContextStub.called, 'setContext called')
   t.deepEqual(setContextStub.args[0][0], mockedRcConfig)
-  loginRewireAPI.__ResetDependency__('setContext')
 })
 
 test.serial('login - user abort', async (t) => {
   confirmationStub.resolves(false)
 
-  emptyContext()
   await loginHandler()
 
-  t.true(readFileStub.called, 'did load rc config')
+  t.true(getContextStub.called, 'did call getContext for rc')
   t.true(confirmationStub.called, 'called confirmation')
   // this depends on process.platform
   // t.true(opnStub.notCalled, 'did not try to open a browser')
-  t.true(writeFileStub.notCalled, 'did not try to store rc')
+  t.true(setContextStub.notCalled, 'did not call setContext to store rc')
   confirmationStub.resolves(true)
 })
 
 test.serial('login - already logged in', async (t) => {
-  loginRewireAPI.__Rewire__('getContext', stub().resolves({cmaToken: 'alreadyLoggedIn'}))
+  getContextStub.resolves({cmaToken: 'alreadyLoggedIn'})
   await loginHandler()
 
-  t.true(readFileStub.notCalled, 'did not try to load rc config')
   t.true(opnStub.notCalled, 'did not try to open a browser')
-  t.true(writeFileStub.notCalled, 'did not try to store rc')
+  t.true(setContextStub.notCalled, 'did not try to setContext to store rc')
   t.true(promptStub.notCalled, 'did not show any inquirer')
-  loginRewireAPI.__ResetDependency__('getContext')
 })
