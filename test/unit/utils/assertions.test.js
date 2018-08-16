549dd3590c56afc3838ca5ac5050e6d5f5f331ba
Benedikt Rötsch <benedikt@contentful.com>
2018-08-16 13:22:19 +0200

    async

--- test/unit/utils/assertions.test.js
+++ test/unit/utils/assertions.test.js
@@ -1,70 +1,43 @@
-import { spy } from 'sinon'
+import { assertLoggedIn, assertSpaceIdProvided } from '../../../lib/utils/assertions'
 
-import {
-  assertLoggedIn,
-  assertSpaceIdProvided,
-  __RewireAPI__ as assertionsRewireAPI
-} from '../../../lib/utils/assertions'
-import {
-  emptyContext,
-  setContext
-} from '../../../lib/context'
-import { PreconditionFailedError } from '../../../lib/utils/error'
+import { getContext } from '../../../lib/context'
+import { highlightStyle } from '../../../lib/utils/styles'
 
-const highlightStyleSpy = spy()
+jest.mock('../../../lib/context')
+jest.mock('../../../lib/utils/styles')
 
-beforeAll(() => {
-  assertionsRewireAPI.__Rewire__('highlightStyle', highlightStyleSpy)
-})
-
-afterAll(() => {
-  assertionsRewireAPI.__ResetDependency__('highlightStyle')
+getContext.mockResolvedValue({
+  cmaToken: 'mockedToken'
 })
 
 afterEach(() => {
-  highlightStyleSpy.resetHistory()
+  highlightStyle.mockClear()
+  getContext.mockClear()
 })
 
 test('assertLoggedIn when not logged in', async () => {
-  await emptyContext()
-  await setContext({})
-  try {
-    await expect(assertLoggedIn()).rejects.toThrowError(PreconditionFailedError)
-  } catch (error) {
-    expect(error.message.includes('You have to be logged in to do this')).toBeTruthy()
-  }
+  getContext.mockResolvedValueOnce({})
+  await expect(assertLoggedIn()).rejects.toThrowErrorMatchingSnapshot()
 })
 
 test('assertLoggedIn when logged in', async () => {
-  await emptyContext()
-  await setContext({
-    cmaToken: 'mocked token'
-  })
-  await expect(assertLoggedIn).not.toThrowError('does not throw error ')
+  await expect(assertLoggedIn)
 })
 
 test('assertSpaceIdProvided when provided via args', async () => {
-  await emptyContext()
-  await setContext({})
-  await expect(() => assertSpaceIdProvided({
+  await assertSpaceIdProvided({
     spaceId: 'mocked spaceId'
-  })).not.toThrowError('does not throw error ')
+  })
 })
 
 test('assertSpaceIdProvided when provided via context', async () => {
-  await emptyContext()
-  await setContext({
-    activeSpaceId: 'mocked spaceId'
+  getContext.mockResolvedValueOnce({
+    activeSpaceId: 'space id',
+    cmaToken: 'mockedToken'
   })
-  await expect(assertSpaceIdProvided).not.toThrowError('does not throw error ')
+  await assertSpaceIdProvided()
 })
 
 test('assertSpaceIdProvided when not provided at all', async () => {
-  await emptyContext()
-  await setContext({})
-  try {
-    await expect(assertSpaceIdProvided({})).rejects.toThrowError(PreconditionFailedError)
-  } catch (error) {
-    expect(error.message.includes('You need to provide a space id')).toBeTruthy()
-  }
+  await expect(assertSpaceIdProvided({})).rejects.toThrowErrorMatchingSnapshot()
 })
