200c2a1e29445077d5b1795385bb68b18879f545
Khaled Garbaya <khaled@contentful.com>
2018-08-30 13:49:36 +0200

    test(space-create): Update tests

--- test/integration/cmds/space/create.test.js
+++ test/integration/cmds/space/create.test.js
@@ -49,6 +49,8 @@ test('should print help message', done => {
 test('should create space', done => {
   app()
     .run(`space create --name cli_test_org_space --organization-id ${process.env.CLI_E2E_ORG_ID}`)
+    .on(/Do you want to confirm the space creation?/)
+    .respond('\n')
     .expect((result) => {
       const resultText = result.stdout.trim()
       var spaceId = extractSpaceId(resultText)
@@ -63,6 +65,36 @@ test('should create space', done => {
 test('should create space using shortcuts args', done => {
   app()
     .run(`space create -n cli_test_org_space_sh --org ${process.env.CLI_E2E_ORG_ID}`)
+    .on(/Do you want to confirm the space creation?/)
+    .respond('\n')
+    .expect((result) => {
+      const resultText = result.stdout.trim()
+      var spaceId = extractSpaceId(resultText)
+      spacesToDelete.push(spaceId)
+      const regex = /Successfully created space .*/ // name doesn't matter because response is replayed
+      expect(result.stdout.trim()).toMatch(regex)
+    })
+    .code(0)
+    .end(done)
+})
+
+test('should should abort space creation when answering no', done => {
+  app()
+    .run(`space create --name cli_test_org_space --organization-id ${process.env.CLI_E2E_ORG_ID}`)
+    .on(/Do you want to confirm the space creation?/)
+    .respond('n\n')
+    .expect((result) => {
+      const resultText = result.stdout.trim()
+      const regex = /Space creation aborted.*/ // name doesn't matter because response is replayed
+      expect(resultText).toMatch(regex)
+    })
+    .code(0)
+    .end(done)
+})
+
+test('should should skip prompt when --yes is set', done => {
+  app()
+    .run(`space create --name cli_test_org_space --yes --organization-id ${process.env.CLI_E2E_ORG_ID}`)
     .expect((result) => {
       const resultText = result.stdout.trim()
       var spaceId = extractSpaceId(resultText)
