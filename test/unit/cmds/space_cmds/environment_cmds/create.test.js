b814d0aaf3d8c98c71e1fe038227dcadd9db9779
Phoebe Schmidt <phbschmidt@gmail.com>
2019-02-19 22:00:32 +0100

    update unit tests

--- test/unit/cmds/space_cmds/environment_cmds/create.test.js
+++ test/unit/cmds/space_cmds/environment_cmds/create.test.js
@@ -60,3 +60,18 @@ test('create new environment with id and name', async () => {
   expect(createEnvironmentWithIdStub.mock.calls[0][0]).toBe('test')
   expect(createEnvironmentWithIdStub.mock.calls[0][1]).toEqual({ name: 'test' })
 })
+
+test.only('create new environment with id and name and source', async () => {
+  const result = await environmentCreate({
+    spaceId: 'someSpaceID',
+    environmentId: 'test',
+    name: 'test',
+    source: 'srcEnv'
+  })
+  expect(result).toBeTruthy()
+  expect(createManagementClient).toHaveBeenCalledTimes(1)
+  expect(createEnvironmentWithIdStub).toHaveBeenCalledTimes(1)
+  expect(createEnvironmentWithIdStub.mock.calls[0][0]).toBe('test')
+  expect(createEnvironmentWithIdStub.mock.calls[0][1]).toEqual({ name: 'test' })
+  expect(createEnvironmentWithIdStub.mock.calls[0][2]).toEqual('srcEnv')
+})
