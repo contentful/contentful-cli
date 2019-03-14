564d64896554280b9cddbfe228b7ed4a6f06d2ca
Makinwa Olubukola <makinwa37@gmail.com>
2019-03-14 14:22:27 +0100

    feat(content-type): enhance list command functionality (#129)

--- docs/content-type/list/README.md
+++ docs/content-type/list/README.md
@@ -8,18 +8,20 @@ Prints the Content Types in a space
 Usage: contentful content-type list [options]
 
 Options:
-  --space-id  Space id                                                  [string]
+  --space-id, -s            Space id                                                  [string]
+  --management-token, -mt   Management token                                          [string]
+  --environment-id, -e      Environment id                                            [string]
 ```
 
 ### Example
 
 ```shell
-contentful content-type list --space-id xxx
+contentful content-type list --space-id xxx -e master
+
+Environment: "master"
 ┌───────────────────┬────────────────────────┐
 │ Content Type Name │ Content Type ID        │
 ├───────────────────┼────────────────────────┤
 │ Test              │ test                   │
 └───────────────────┴────────────────────────┘
 ```
-
-
---
+++ test/unit/cmds/content-type_cmds/list.test.js
@@ -0,0 +1,138 @@
+import { handler } from '../../../../lib/cmds/content-type_cmds/list'
+import { getContext } from '../../../../lib/context'
+import { log } from '../../../../lib/utils/log'
+import { createManagementClient } from '../../../../lib/utils/contentful-clients'
+
+jest.mock('../../../../lib/context')
+jest.mock('../../../../lib/utils/log')
+jest.mock('../../../../lib/utils/contentful-clients')
+
+const mockContentTypes = [
+  {
+    name: 'content type name',
+    sys: {
+      id: 'mockedMasterCT',
+      environment: {
+        sys: {
+          id: 'master'
+        }
+      }
+    }
+  },
+  {
+    name: 'content type name',
+    sys: {
+      id: 'mockedDevelopCT',
+      environment: {
+        sys: {
+          id: 'develop'
+        }
+      }
+    }
+  },
+  {
+    name: 'content type name',
+    sys: {
+      id: 'mockedTestCT',
+      environment: {
+        sys: {
+          id: 'test'
+        }
+      }
+    }
+  }
+]
+
+const getContentTypesSub = jest.fn().mockResolvedValue({
+  items: mockContentTypes
+})
+
+const fakeClient = {
+  getSpace: async () => ({
+    getEnvironment: async () => ({
+      getContentTypes: getContentTypesSub
+    })
+  })
+}
+createManagementClient.mockResolvedValue(fakeClient)
+
+getContext.mockResolvedValue({
+  cmaToken: 'mockedToken',
+  activeSpaceId: 'someSpaceId'
+})
+
+afterEach(() => {
+  createManagementClient.mockClear()
+  getContentTypesSub.mockClear()
+  log.mockClear()
+})
+
+test('List content types from default environment, "master"', async () => {
+  await handler({})
+
+  expect(createManagementClient).toHaveBeenCalledTimes(1)
+  expect(getContentTypesSub).toHaveBeenCalledTimes(1)
+
+  expect(log.mock.calls[0][0]).toContain(
+    mockContentTypes[0].sys.environment.sys.id
+  )
+  expect(log.mock.calls[0][0]).not.toContain(
+    mockContentTypes[1].sys.environment.sys.id
+  )
+  expect(log.mock.calls[0][0]).not.toContain(
+    mockContentTypes[2].sys.environment.sys.id
+  )
+
+  expect(log.mock.calls[1][0]).toContain(mockContentTypes[0].name)
+  expect(log.mock.calls[1][0]).toContain(mockContentTypes[0].sys.id)
+})
+
+test('List content types based on active environment if available', async () => {
+  getContext.mockResolvedValue({
+    cmaToken: 'mockedToken',
+    activeSpaceId: 'someSpaceId',
+    activeEnvironmentId: 'develop'
+  })
+
+  await handler({})
+
+  expect(createManagementClient).toHaveBeenCalledTimes(1)
+  expect(getContentTypesSub).toHaveBeenCalledTimes(1)
+
+  expect(log.mock.calls[0][0]).not.toContain(
+    mockContentTypes[0].sys.environment.sys.id
+  )
+  expect(log.mock.calls[0][0]).toContain(
+    mockContentTypes[1].sys.environment.sys.id
+  )
+  expect(log.mock.calls[0][0]).not.toContain(
+    mockContentTypes[2].sys.environment.sys.id
+  )
+
+  expect(log.mock.calls[1][0]).toContain(mockContentTypes[1].name)
+  expect(log.mock.calls[1][0]).toContain(mockContentTypes[1].sys.id)
+})
+
+test('List content types based on environment passed if --environment-id option is used', async () => {
+  const stubArgv = {
+    environmentId: 'test'
+  }
+
+  await handler(stubArgv)
+
+  expect(createManagementClient).toHaveBeenCalledTimes(1)
+  expect(getContentTypesSub).toHaveBeenCalledTimes(1)
+
+  expect(log.mock.calls[0][0]).not.toContain(
+    mockContentTypes[0].sys.environment.sys.id
+  )
+  expect(log.mock.calls[0][0]).not.toContain(
+    mockContentTypes[1].sys.environment.sys.id
+  )
+  expect(log.mock.calls[0][0]).toContain(
+    mockContentTypes[2].sys.environment.sys.id
+  )
+
+  expect(log.mock.calls[1][0]).toContain(mockContentTypes[2].name)
+  expect(log.mock.calls[1][0]).toContain(mockContentTypes[2].sys.id)
+})
