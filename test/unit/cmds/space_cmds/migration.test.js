9f1e19418642abcabb9464871c5ed659d81510ae
Thomas Scholtes <thomas-scholtes@gmx.de>
2019-06-04 18:22:24 +0200

    fix(migration): Pass access token correctly to migration lib (#142)
    Before, the migration CLI would not pick up the access token passed from
    `contentful-cli`, instead it would use its own mechanism to resolve
    `~/.contentfulrc.json`. This was caused by the migration CLI using a
    different property name for the access token [1].
    
    [1]: https://github.com/contentful/contentful-migration/blob/master/src/bin/lib/config.ts#L9

--- test/unit/cmds/space_cmds/migration.test.js
+++ test/unit/cmds/space_cmds/migration.test.js
@@ -11,7 +11,7 @@ getContext.mockResolvedValue({ cmaToken: 'managementToken' })
 
 test('it should pass all args to the migration', async () => {
   const stubArgv = {
-    managementToken: 'managementToken',
+    accessToken: 'managementToken',
     managementApplication: `contentful.cli/${version}`,
     spaceId: 'spaceId',
     managementFeature: 'space-migration'
