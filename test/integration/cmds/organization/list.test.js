b7fcab829305e6cab63b6ae89a9ed4800449f5ee
jroehl <mail@johannroehl.de>
2019-06-28 13:32:40 +0200

    Added integration tests

---
+++ test/integration/cmds/organization/list.test.js
@@ -0,0 +1,38 @@
+import nixt from 'nixt'
+import { join } from 'path'
+import { initConfig } from '../../util'
+
+const bin = join(__dirname, './../../../../', 'bin')
+const org = process.env.CLI_E2E_ORG_ID
+
+const app = () => {
+  return nixt({ newlines: true })
+    .cwd(bin)
+    .base('./contentful.js ')
+    .clone()
+}
+
+beforeAll(() => {
+  return initConfig()
+})
+
+test('should print help message', done => {
+  app()
+    .run('organization list --help')
+    .code(0)
+    .expect(result => {
+      const resultText = result.stdout.trim()
+      expect(resultText).toMatchSnapshot('help data is incorrect')
+    })
+    .end(done)
+})
+
+test('should list organizations', done => {
+  app()
+    .run('organization list')
+    .code(0)
+    .stdout(/(Organization name)|(Organization id)/)
+    .stdout(RegExp(org))
+    .stdout(/Contentful CLI Test Org/)
+    .end(done)
+})
