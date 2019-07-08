0a94c003e7d428ba252db2baf3fffe346fefa557
jroehl <johann.roehl@contentful.com>
2019-07-08 09:36:08 +0200

    Bump formula only for master branch

--- script/bump-brew-formula.sh
+++ script/bump-brew-formula.sh
@@ -7,9 +7,6 @@ command -v brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.gi
 brew update
 brew install coreutils
 
-DRY_RUN="--dry-run"
-if [ "${CIRCLE_BRANCH}" == "master" ]; then DRY_RUN=""; fi
-
 if [ ! $(git config --global user.email) ]; then git config --global user.email "support@contentful.com"; fi
 if [ ! $(git config --global user.name) ]; then git config --global user.name "contentful"; fi
 
@@ -18,4 +15,4 @@ version=$(npm show ${package} version)
 url="https://registry.npmjs.org/${package}/-/${package}-${version}.tgz"
 sha256=$(curl -s ${url} | gsha256sum | awk '{ print $1 }')
 
-brew bump-formula-pr --strict ${package} --url=${url} --sha256=${sha256} --no-browse --message="This PR was automatically created via a script. Contact @contentful with any questions." $DRY_RUN || { if [ "${DRY_RUN}" != "" ]; then echo "Finished ${DRY_RUN}."; else exit 1; fi; }
+brew bump-formula-pr --strict ${package} --url=${url} --sha256=${sha256} --no-browse --message="This PR was automatically created via a script. Contact @contentful with any questions."
