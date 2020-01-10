72a9cb96e4c3a6530583dffe1a1bd1e5ec0b0ed3
Janko Marklein <janko.marklein@contentful.com>
2020-01-10 11:26:05 +0100

    Add test

--- test/integration/cmds/space/delete.test.js
+++ test/integration/cmds/space/delete.test.js
@@ -1,6 +1,9 @@
 const nixt = require('nixt')
 const { join } = require('path')
 const { initConfig, createSimpleSpace } = require('../../util')
+const { readFile, writeFile } = require('mz/fs')
+const { homedir } = require('os')
+const { resolve } = require('path')
 
 const bin = join(__dirname, './../../../../', 'bin')
 const org = process.env.CLI_E2E_ORG_ID
@@ -21,6 +24,17 @@ beforeAll(async () => {
   space = await createSimpleSpace(org, 'space-delete')
 })
 
+test('should print help message', done => {
+  app()
+    .run('space delete --help')
+    .code(0)
+    .expect(result => {
+      const resultText = result.stdout.trim()
+      expect(resultText).toMatchSnapshot('help data is incorrect')
+    })
+    .end(done)
+})
+
 test('should exit 1 when no args', done => {
   app()
     .run('space delete')
@@ -32,14 +46,43 @@ test('should exit 1 when no args', done => {
     .end(done)
 })
 
-test('should print help message', done => {
+test('should exit 1 when no args, even with activeSpaceId set', async done => {
+  // Add and remove options activeSpaceId and activeEnvironmentId to
+  // .contentfulrc.json for only this test:
+  const configFilePath = resolve(homedir(), '.contentfulrc.json')
+  let configContents = null
+  async function before() {
+    try {
+      let configContentsBuffer = await readFile(configFilePath)
+      configContents = JSON.parse(configContentsBuffer)
+      return writeFile(
+        configFilePath,
+        JSON.stringify(
+          {
+            ...configContents,
+            activeSpaceId: space.sys.id,
+            activeEnvironmentId: 'master'
+          },
+          null,
+          2
+        )
+      )
+    } catch (e) {
+      throw Error(e)
+    }
+  }
+  async function after() {
+    return writeFile(configFilePath, configContents)
+  }
   app()
-    .run('space delete --help')
-    .code(0)
+    .before(before)
+    .run(`space delete --yes`)
     .expect(result => {
-      const resultText = result.stdout.trim()
-      expect(resultText).toMatchSnapshot('help data is incorrect')
+      const regex = /space was successfully deleted/
+      expect(result.stdout.trim()).not.toMatch(regex)
     })
+    .code(1)
+    .after(after)
     .end(done)
 })
 
