0193db4093480b57866b829efb434b948fbed7e8
Ahmed T. Ali <ah.tajelsir@gmail.com>
2021-04-07 09:54:02 +0200

    test: fix failing unit tests

--- test/unit/utils/headers.test.js
+++ test/unit/utils/headers.test.js
@@ -1,5 +1,9 @@
 const { getHeadersFromOption } = require('../../../lib/utils/headers')
 
+test('getHeadersFromOption returns empty object when value is undefined', () => {
+  expect(getHeadersFromOption(undefined)).toEqual({})
+})
+
 test('getHeadersFromOption accepts single or multiple values', () => {
   expect(getHeadersFromOption('Accept: Any')).toEqual({ Accept: 'Any' })
   expect(getHeadersFromOption(['Accept: Any', 'X-Version: 1'])).toEqual({
