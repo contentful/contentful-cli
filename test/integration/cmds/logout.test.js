790ca87647f4cacc3a61a355a884535bd2c29070
Moe Shaaban <info@mshaaban.com>
2022-07-20 15:09:39 +0200

    test: update snapshots

--- test/integration/cmds/logout.test.js
+++ test/integration/cmds/logout.test.js
@@ -12,7 +12,7 @@ beforeAll(() => {
   return initConfig()
 })
 
-test.only('should not logout', done => {
+test('should not logout', done => {
   app()
     .run('logout')
     .on(/Do you want to log out now\?/)
