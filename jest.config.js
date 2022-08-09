1703f7522da82cabc240bed7bf32183714dcac19
mshaaban0 <info@mshaaban.com>
2022-08-09 21:09:06 +0200

    test: init ts-jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testEnvironmentOptions: {
+    url: 'http://localhost/'
+  },
+  transformIgnorePatterns: ['node_modules/(?!(.+))']
+  // Use this to set env variables for local development
+  // setupFiles: ['<rootDir>/.jest/env.js']
+}
