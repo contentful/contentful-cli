d5ee468cf314682ff783d8f91a79cae8afe2057c
Renato Massao Yonamine <massao.yonamine@contentful.com>
2022-08-10 15:57:44 +0200

    feat: add create new space and populate content to init

--- lib/cmds/init.ts
+++ lib/cmds/init.ts
@@ -1,8 +1,12 @@
+import inquirer from 'inquirer'
 import { Argv } from 'yargs'
 import { handleAsyncError as handle } from '../utils/async'
 import greetings from './init/greetings'
 import { getContext } from '../context'
 import { login } from './login'
+import { spaceCreate } from './space_cmds/create'
+import { importSpace } from './space_cmds/import'
+import initialContent from './init/content.json'
 
 export const command = 'init'
 
@@ -15,14 +19,63 @@ export const builder = (yargs: Argv) => {
 }
 
 export const init = async () => {
-  const context = await getContext()
-
   greetings()
 
-  if (!context.managementToken)
-    await login({
-      context
+  const context = await getContext()
+  const managementToken = context.managementToken
+  await login({
+    context,
+    managementToken
+  })
+
+  const { newSpace } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'newSpace',
+      message: 'Do you want to create a new space or use an existing one?',
+      choices: [
+        {
+          name: 'Create new space',
+          value: true
+        },
+        {
+          name: 'Use existing space',
+          value: false
+        }
+      ]
+    }
+  ])
+  if (newSpace) {
+    const { spaceName, content } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'spaceName',
+        message: 'What should be the name for the new created space?',
+        validate: name => name !== '' || 'Space name is required'
+      },
+      {
+        type: 'confirm',
+        name: 'content',
+        message: ({ spaceName }) =>
+          `Do you want to have example content in ${spaceName}?`
+      }
+    ])
+
+    const space = await spaceCreate({
+      context,
+      name: spaceName
     })
+
+    if (content) {
+      await importSpace({
+        context: {
+          ...context,
+          activeSpaceId: space.sys.id
+        },
+        content: initialContent
+      })
+    }
+  }
 }
 
 export const handler = handle(init)
