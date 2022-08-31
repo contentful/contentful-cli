10a6254f04788a5702d63861cf6a2d707086f0b9
Renato Massao Yonamine <1071799+massao@users.noreply.github.com>
2022-08-31 15:54:59 +0200

    feat: add create new space and populate content [BAU-890] (#1583)
    * feat: add create new space and populate content to init
    
    * refactor: move create space to typescript and remove defaultLocale
    
    defaultLocale option was being passed to the client.createSpace, but on
    the CMA it doesn't have that on the type definition of the funciton.
    
    * refactor: migrate space import to typescript
    
    * fix: fix typescript errors
    
    * fix: new output path
    
    * fix: add token check on init command
    
    * test: update space create snapshot and fix init test
    
    * refactor: add condition to only call login if no managementToken is on context
    
    Co-authored-by: mshaaban0 <info@mshaaban.com>

--- test/integration/cmds/init.test.js
+++ test/integration/cmds/init.test.js
@@ -9,9 +9,9 @@ const app = () =>
 test('init: should print greeting message', done => {
   app()
     .run('init')
-    .code(0)
     .stdout(/Welcome to Contentful/)
-    .on(/You are not logged in, continue login in browser?/)
-    .respond('n\n')
+    // Skip the list prompt, as it makes the test hangs, it seems fine if it's a string prompt
+    .on(/Do you want to create a new space or use an existing one?/)
+    .respond('\n')
     .end(done)
 })
