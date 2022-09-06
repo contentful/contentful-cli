e9851c323db74ec91325ac4e39b03c8f520b181a
Moe Shaaban <mshaaban0@users.noreply.github.com>
2022-09-06 16:05:38 +0200

    chore: clean up login command messaging structure (#1613)

--- lib/utils/async.ts
+++ lib/utils/async.ts
@@ -1,13 +1,13 @@
-const { logError } = require('./log')
+import { Argv } from 'yargs'
+import { logError } from './log'
 
-module.exports.handleAsyncError =
-  (asyncFn, errorHandler = logError) =>
-  argv => {
-    return asyncFn(argv).catch(error => {
+export const handleAsyncError =
+  (asyncFn: (params: any) => Promise<unknown>, errorHandler = logError) =>
+  (argv: Argv) =>
+    asyncFn(argv).catch((error: Error) => {
       errorHandler(error)
       // Since the error got catched to allow async commands in yargs
       // and we can't throw to avoid unresolved promise rejections,
       // we manually exit here with exit code 1
       process.exit(1)
     })
-  }
