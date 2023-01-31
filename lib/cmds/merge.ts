df114d958c0933f9dab2e9927f4b274f0ee5d174
Christian Stoyanov <cristian.stoyanov@gmail.com>
2023-01-31 12:24:01 +0100

    Feat: Add `merge` command and initial `export` subcommand (#1806)

---
+++ lib/cmds/merge.ts
@@ -0,0 +1,12 @@
+import type { Argv } from 'yargs'
+
+module.exports.command = 'merge'
+
+module.exports.desc = 'A CLI version of the merge app'
+
+module.exports.builder = function (yargs: Argv) {
+  return yargs
+    .usage('')
+    .commandDir('merge_cmds')
+    .demandCommand(4, 'Please specify a sub command.')
+}
