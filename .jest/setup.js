ca846c99709fce98e78411e00a99831153671c03
Moe Shaaban <info@mshaaban.com>
2023-02-17 17:31:35 +0100

    test: fix useFakeTimers to mock date

--- .jest/setup.js
+++ .jest/setup.js
@@ -2,6 +2,9 @@ import { cleanUpTestSpaces } from '@contentful/integration-test-utils'
 import { initConfig } from '../test/contentful-config'
 
 beforeAll(() => {
+  jest.useFakeTimers('modern')
+  jest.setSystemTime(new Date(2013, 1, 1))
+
   return initConfig()
 })
 
