216eef7493578d9d2c41cbcd9a6dc2fce049fb57
Christian Stoyanov <christian.stoyanov@contentful.com>
2023-03-03 10:56:43 +0100

    feat: add `merge export` command [SDK-2501] (#1851)
    Co-authored-by: Janko Marklein <janko.marklein@contentful.com>

--- test/unit/cmds/merge_cmds/export.test.ts
+++ test/unit/cmds/merge_cmds/export.test.ts
@@ -1,13 +1,18 @@
 import { PlainClientAPI } from 'contentful-management'
 
 import * as appInstallUtils from '../../../../lib/utils/app-installation'
+import * as exportCmd from '../../../../lib/cmds/merge_cmds/export'
 
 const mockedClient = {
   appInstallation: {
     get: jest.fn()
   },
   raw: {
-    put: jest.fn()
+    put: jest.fn(),
+    getDefaultParams: jest.fn()
+  },
+  environmentAlias: {
+    getMany: jest.fn().mockResolvedValue({ items: [] })
   }
 } as unknown as PlainClientAPI
 
@@ -61,5 +66,39 @@ describe('merge export command', () => {
 
     expect(appInstalledInBothEnvs).toBe(true)
     expect(mockedClient.appInstallation.get).toHaveBeenCalledTimes(2)
+  })
+
+  it('calls calls the create changeset and export migration actions', async () => {
+    mockedClient.appActionCall = {
+      create: jest
+        .fn()
+        .mockResolvedValueOnce({
+          sys: {
+            id: 'action-id-create-changeset'
+          }
+        })
+        .mockResolvedValueOnce({
+          sys: {
+            id: 'action-id-export-migration'
+          }
+        }),
+      getCallDetails: jest.fn().mockResolvedValueOnce({
+        statusCode: 200,
+        response: {
+          body: '{"message": {"migration":"console.log(\'hello world\')"}}'
+        }
+      })
+    }
+
+    await exportCmd.callExportAppAction({
+      api: mockedClient,
+      appDefinitionId: 'app-id',
+      appActionId: 'action-id',
+      sourceEnvironmentId: 'source',
+      targetEnvironmentId: 'target',
+      spaceId: 'space'
+    })
+
+    expect(mockedClient.appActionCall.create).toHaveBeenCalledTimes(2)
   })
 })
