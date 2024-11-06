69ad55623ed8a11173f71672a3156a858cd3661d
Maya <maya.karabulastysiak@gmail.com>
2024-11-06 21:10:42 +0100

    fix: fix column references in example test

--- test/integration/cmds/organization/example-load.js
+++ test/integration/cmds/organization/example-load.js
@@ -9,19 +9,19 @@ module.exports = async function ({ csv, taxonomy, fs }) {
   const { data } = await csv.parse(csvFile.toString(), { header: true })
 
   for (const row of data) {
-    taxonomy.addConcept(row['Animal Group'], {
-      prefLabel: row['Animal Group']
+    taxonomy.addConcept(row['Animal-Group'], {
+      prefLabel: row['Animal-Group']
     })
 
     const subCategory = taxonomy.addConcept(row['Sub-Category'], {
       prefLabel: row['Sub-Category']
     })
     if (subCategory) {
-      subCategory.addBroader(row['Animal Group'])
+      subCategory.addBroader(row['Animal-Group'])
     }
 
-    const animal = taxonomy.addConcept(row['Animal Name'], {
-      prefLabel: row['Animal Name'],
+    const animal = taxonomy.addConcept(row['Animal-Name'], {
+      prefLabel: row['Animal-Name'],
       descritpion: row['Skills']
     })
     if (animal) {
