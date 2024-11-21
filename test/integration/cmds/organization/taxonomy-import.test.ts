ab29bddba054acab2360a9ee176a2086a6aafb78
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2024-11-21 13:52:11 +0000

    test: outlined scenarios

---
+++ test/integration/cmds/organization/taxonomy-import.test.ts
@@ -0,0 +1,17 @@
+/* 
+    should print help message
+
+    should print help message when correct arguments are not provided
+
+    should create concept if doesn't exist
+        ensure we check relations too
+    
+    should create concept-scheme if doesn't exist
+        also check top concepts
+    
+    should update concept if already exists
+        ensure we check relations too
+    
+    should update concept-scheme if already exists
+        also check top concepts
+*/
