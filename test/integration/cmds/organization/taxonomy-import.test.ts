944783e12c765ce64ab5104a6248e02f29a5b891
vida momenzadeh <vida.momenzadeh@contentful.com>
2025-03-24 12:46:56 +0100

    update to check which test is failing

--- test/integration/cmds/organization/taxonomy-import.test.ts
+++ test/integration/cmds/organization/taxonomy-import.test.ts
@@ -47,73 +47,50 @@ describe('organization import', () => {
     ])
   })
 
-  test('should print help message', done => {
-    app()
-      .run(`${cmd} --help`)
-      .code(0)
-      .expect(result => {
-        const text = result.stdout.trim()
-        expect(text).toMatchSnapshot('organization import help data is correct')
-      })
-      .end(done)
-  })
+  // test(`should create concepts and scheme if doesn't exist`, done => {
+  //   app()
+  //     .run(
+  //       `${cmd} --organization-id ${organizationId} --content-file ../test/integration/cmds/organization/example-taxonomy.json`
+  //     )
+  //     .expect(async ({ stdout }: Result) => {
+  //       const resultText = stdout.trim()
 
-  test('should print help message when correct arguments are not provided', done => {
-    app()
-      .run(`${cmd}`)
-      .code(1)
-      .expect(({ stderr }: Result) => {
-        const resultText = stderr.trim()
+  //       expect(resultText).toContain('Create concepts')
+  //       expect(resultText).toContain('Add concept relations')
+  //       expect(resultText).toContain('Create concept schemes')
+  //     })
+  //     .end(async () => {
+  //       const [concept0, concept1, concept2, scheme0] = await Promise.all([
+  //         cmaClient.concept.get({
+  //           conceptId: 'concept0',
+  //           organizationId
+  //         }),
+  //         cmaClient.concept.get({
+  //           conceptId: 'concept1',
+  //           organizationId
+  //         }),
+  //         cmaClient.concept.get({
+  //           conceptId: 'concept2',
+  //           organizationId
+  //         }),
+  //         cmaClient.conceptScheme.get({
+  //           conceptSchemeId: 'scheme0',
+  //           organizationId
+  //         })
+  //       ])
 
-        expect(resultText).toContain('Usage: contentful organization import')
-      })
-      .end(done)
-  })
+  //       expect(concept0.prefLabel['en-US']).toContain('Animals')
+  //       expect(concept1.prefLabel['en-US']).toContain('Primates')
+  //       expect(concept2.prefLabel['en-US']).toContain('Plants')
+  //       expect(scheme0.prefLabel['en-US']).toContain('Scheme')
 
-  test(`should create concepts and scheme if doesn't exist`, done => {
-    app()
-      .run(
-        `${cmd} --organization-id ${organizationId} --content-file ../test/integration/cmds/organization/example-taxonomy.json`
-      )
-      .expect(async ({ stdout }: Result) => {
-        const resultText = stdout.trim()
+  //       expect(concept1.broader[0].sys.id).toContain(concept0.sys.id)
+  //       expect(concept0.related[0].sys.id).toContain(concept2.sys.id)
+  //       expect(concept2.related[0].sys.id).toContain(concept0.sys.id)
 
-        expect(resultText).toContain('Create concepts')
-        expect(resultText).toContain('Add concept relations')
-        expect(resultText).toContain('Create concept schemes')
-      })
-      .end(async () => {
-        const [concept0, concept1, concept2, scheme0] = await Promise.all([
-          cmaClient.concept.get({
-            conceptId: 'concept0',
-            organizationId
-          }),
-          cmaClient.concept.get({
-            conceptId: 'concept1',
-            organizationId
-          }),
-          cmaClient.concept.get({
-            conceptId: 'concept2',
-            organizationId
-          }),
-          cmaClient.conceptScheme.get({
-            conceptSchemeId: 'scheme0',
-            organizationId
-          })
-        ])
-
-        expect(concept0.prefLabel['en-US']).toContain('Animals')
-        expect(concept1.prefLabel['en-US']).toContain('Primates')
-        expect(concept2.prefLabel['en-US']).toContain('Plants')
-        expect(scheme0.prefLabel['en-US']).toContain('Scheme')
-
-        expect(concept1.broader[0].sys.id).toContain(concept0.sys.id)
-        expect(concept0.related[0].sys.id).toContain(concept2.sys.id)
-        expect(concept2.related[0].sys.id).toContain(concept0.sys.id)
-
-        done()
-      })
-  })
+  //       done()
+  //     })
+  // })
 
   // test(`should update concept and scheme`, done => {
   //   app()
