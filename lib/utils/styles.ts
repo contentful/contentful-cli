f4cd4dafe533f98ca8fe0a1e2f3bcc35c441229f
Ely Lucas <ely@meta-tek.net>
2025-04-14 15:27:46 -0600

    chore: convert styles.js content to TypeScript

--- lib/utils/styles.ts
+++ lib/utils/styles.ts
@@ -1,14 +1,15 @@
-const chalk = require('chalk')
+import chalk from 'chalk'
 
-chalk.enabled = process.env.NODE_ENV !== 'test'
+// Disable colors in test environment
+chalk.level = process.env.NODE_ENV !== 'test' ? chalk.level : 0
 
-module.exports.successStyle = chalk.green
-module.exports.warningStyle = chalk.yellow
-module.exports.errorStyle = chalk.red
-module.exports.highlightStyle = chalk.cyan
-module.exports.headingStyle = chalk.cyan.bold
-module.exports.codeStyle = chalk.dim
-module.exports.pathStyle = chalk.cyan.underline
-module.exports.pathTextStyle = chalk.blue
-module.exports.blockQuoteStyle = chalk.magenta.italic
-module.exports.htmlStyle = chalk.green
+export const successStyle = chalk.green
+export const warningStyle = chalk.yellow
+export const errorStyle = chalk.red
+export const highlightStyle = chalk.cyan
+export const headingStyle = chalk.cyan.bold
+export const codeStyle = chalk.dim
+export const pathStyle = chalk.cyan.underline
+export const pathTextStyle = chalk.blue
+export const blockQuoteStyle = chalk.magenta.italic
+export const htmlStyle = chalk.green
