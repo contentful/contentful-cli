ea4f81428bae42605c3c1d1bfb12b35bdcf364a3
Ely Lucas <ely@meta-tek.net>
2025-04-14 15:17:52 -0600

    chore: convert contentful-clients.js content to TypeScript

--- lib/utils/contentful-clients.ts
+++ lib/utils/contentful-clients.ts
@@ -1,15 +1,29 @@
-const { createClient } = require('contentful-management')
-const { version } = require('../../package.json')
-const { getContext } = require('../context')
-const { agentFromProxy } = require('./proxy')
+import {
+  createClient,
+  PlainClientAPI,
+  ClientAPI,
+  ClientOptions
+} from 'contentful-management'
+import { version } from '../../package.json'
+import { getContext } from '../context'
+import { agentFromProxy } from './proxy'
 
-async function createManagementClient(params) {
+// Extending ClientOptions type but making specific adjustments for compatibility
+type ClientParams = Omit<ClientOptions, 'apiAdapter' | 'accessToken'> & {
+  accessToken?: string | (() => Promise<string>)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  apiAdapter?: any // Using any here to match how the original JS file behaved
+}
+
+export async function createManagementClient(
+  params: ClientParams
+): Promise<ClientAPI> {
   params.application = `contentful.cli/${version}`
 
   const context = await getContext()
   const { rawProxy, proxy, host, insecure } = context
 
-  const proxyConfig = {}
+  const proxyConfig: Record<string, unknown> = {}
   if (!rawProxy) {
     const { httpsAgent } = agentFromProxy(proxy)
     proxyConfig.httpsAgent = httpsAgent
@@ -17,16 +31,24 @@ async function createManagementClient(params) {
     proxyConfig.proxy = proxy
   }
 
-  return createClient({ ...params, ...proxyConfig, host, insecure })
+  return createClient({
+    ...params,
+    ...proxyConfig,
+    host,
+    insecure
+  } as ClientOptions)
 }
 
-async function createPlainClient(params, defaults = {}) {
+export async function createPlainClient(
+  params: ClientParams,
+  defaults: Record<string, unknown> = {}
+): Promise<PlainClientAPI> {
   params.application = `contentful.cli/${version}`
 
   const context = await getContext()
   const { rawProxy, proxy, host, insecure } = context
 
-  const proxyConfig = {}
+  const proxyConfig: Record<string, unknown> = {}
   if (!rawProxy) {
     const { httpsAgent } = agentFromProxy(proxy)
     proxyConfig.httpsAgent = httpsAgent
@@ -35,9 +57,12 @@ async function createPlainClient(params, defaults = {}) {
   }
 
   return createClient(
-    { ...params, ...proxyConfig, host, insecure },
+    {
+      ...params,
+      ...proxyConfig,
+      host,
+      insecure
+    } as ClientOptions,
     { type: 'plain', defaults }
-  )
+  ) as PlainClientAPI
 }
-
-module.exports = { createManagementClient, createPlainClient }
