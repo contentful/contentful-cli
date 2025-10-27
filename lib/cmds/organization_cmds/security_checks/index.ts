b30fd5fb2d48049d7948b3bca04bd70372601df7
hussam.khrais <telehussam@gmail.com>
2025-10-27 17:08:12 +0100

    adding sso enforce check

--- lib/cmds/organization_cmds/security_checks/index.ts
+++ lib/cmds/organization_cmds/security_checks/index.ts
@@ -1,5 +1,10 @@
 import { permissionCheck } from './permission'
 import { ssoEnabledCheck } from './sso_enabled'
+import { ssoEnforcedCheck } from './sso_enforced'
 import type { SecurityCheck } from './types'
 
-export const checks: SecurityCheck[] = [permissionCheck, ssoEnabledCheck]
+export const checks: SecurityCheck[] = [
+  permissionCheck,
+  ssoEnabledCheck,
+  ssoEnforcedCheck
+]
