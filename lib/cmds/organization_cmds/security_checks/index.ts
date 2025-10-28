1f7a585088e65cc3bf98bda577d9f24f0e1fdcbe
hussam.khrais <telehussam@gmail.com>
2025-10-28 09:17:38 +0100

    add security contact check

--- lib/cmds/organization_cmds/security_checks/index.ts
+++ lib/cmds/organization_cmds/security_checks/index.ts
@@ -3,10 +3,12 @@ import { ssoEnabledCheck } from './sso_enabled'
 import { ssoEnforcedCheck } from './sso_enforced'
 import { ssoExemptUsersCheck } from './sso_exempt_users'
 import { ssoExemptUsersWithMfaDisabledCheck } from './sso_exempt_users_with_mfa_disabled'
+import { securityContactSetCheck } from './security_contact_set'
 import type { SecurityCheck } from './types'
 
 export const checks: SecurityCheck[] = [
   permissionCheck,
+  securityContactSetCheck,
   ssoEnabledCheck,
   ssoEnforcedCheck,
   ssoExemptUsersCheck,
